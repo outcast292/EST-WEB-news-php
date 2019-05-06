cc7bf260988b1f2beac92a6321accd528b93c57d
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2019-05-06 17:30:25 +0000

    addin  features to admin index

--- admin/index.php
+++ admin/index.php
@@ -19,6 +19,7 @@
             $row = mysqli_fetch_assoc($result);
             if($_POST["pass"]==$row["pass"]){
             	$_session["id_ad"]=$row["id_ad"];
+            	$_session["name"]=$row["name"];
             }}
 
         if (!isset($_session["id_ad"])) {
@@ -30,7 +31,7 @@
 		
 	?>
 
-	<div style="width: 900px;margin:auto;
+	<div style="width: 900px;margin:auto;height: 900px;
     background-color: lightgray;">
 		<header>
 		<div id="title">
@@ -51,7 +52,73 @@
     </nav>
 
 	</header>
+
+	<h2>Bonjour <?php echo $_session['name']; ?></h2>	
+	<?php 
+	 $sql = "SELECT sum(views) as sum FROM article";
+	 $result = $conn->query($sql);
+     $views = mysqli_fetch_assoc($result);
+     $sql = "SELECT count(*) as count FROM article";
+	 $result = $conn->query($sql);
+     $art = mysqli_fetch_assoc($result);
+     $sql = "SELECT count(*) as count FROM comment";
+	 $result = $conn->query($sql);
+     $cmt = mysqli_fetch_assoc($result);
+
+
+
+
+	 ?>
+	<div class="ct">
+
+		<div class="vue">
+			<legend style="text-decoration: underline;">statistiques en vedette </legend>
+			<br>
+			<table>
+				<tr>
+					<th>views</th>
+					<th>articles</th>
+					<th>commantaires</th>
+				</tr>
+				<tr>
+					<td><?php echo $views["sum"]; ?></td>
+					<td><?php echo $art["count"]; ?></td>
+					<td><?php echo $cmt["count"]; ?></td>
+				</tr>
+			</table>
+		</div>
+		<br>
+	<br>
+	<br>
+	<br>
 	
+		<div class="vue">
+	<?php 
+	$sql = "SELECT id_article,id_admin,contenu,da,label,views FROM article order by id_article desc limit 5";
+	$result = $conn->query($sql);
+     echo "<table>
+				<tr>
+					<th>article id</th>
+					<th>admin id</th>
+					<th>da</th>
+					<th>title</th>
+					<th>views</th>
+				</tr>";
+     while ( $art = mysqli_fetch_assoc($result)){
+     	echo "<tr>
+					<td>".$art['id_article']."</td>
+					<td>".$art['id_admin']."</td>
+					<td>".$art['da']."</td>
+					<td>".$art['label']."</td>
+					<td>".$art['views']."</td>
+				</tr>";
+     }
+	 ?>
+
+	</div>
+	</div>
+	
+
 
 	</div>
 	
