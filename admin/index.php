926dfd66b372a89889e2ecd18ded9ef34a096318
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2019-05-06 16:59:36 +0000

    login system is on

--- admin/index.php
+++ admin/index.php
@@ -5,47 +5,60 @@
 	<title> Admin pannel </title>
 
 </head>
-
 <body>
-<div style="width: 900px;margin: auto;">
+	<?php 
+	session_start();
+		if ((isset($_POST["name"]) && isset($_POST["pass"]) && !isset($_session["id_ad"]))) {
+			$servername = "localhost";
+            $username = "root";
+            $password = "";
+            $p=$_POST["pass"];
+            $conn = new mysqli($servername, $username, $password,'news');
+            $sql = "SELECT * FROM admin where name='root'";
+            $result = $conn->query($sql);
+            $row = mysqli_fetch_assoc($result);
+            if($_POST["pass"]==$row["pass"]){
+            	$_session["id_ad"]=$row["id_ad"];
+            }}
+
+        if (!isset($_session["id_ad"])) {
+        	require 'php/login.php';
+        }
+		
+	 
+	else{
+		
+	?>
+
+	<div style="width: 900px;margin:auto;
+    background-color: lightgray;">
+		<header>
+		<div id="title">
+                    <div id="logo">
+                        <img src="../images/zozor_logo.png" alt="LOGO" />
+                        <h1>HELLO WORLD</h1>    
+                    </div>
+                    <h2>ADMIN PAGE</h2>
+                </div>
+                <nav >
+        <ul>
+
+         <li><a href="#">acceuil</a></li>
+         <li><a href="#">aleatoire</a></li>
+         <li><a href="#">feuilleter</a></li>
+         <li><a href="#">contact</a></li>
+          </ul>
+    </nav>
+
+	</header>
 	
-<div id="header">
-</br>
 
-
-<h1 style="padding-top: 2pxd"> News Now </h1>
-<div class="info"> Root    <input type="button" value="logout" >
-	<p><time>10:00</time>   11/11/1111.</p>
-	<p class="do">last login:   <time>10:00</time> 11/11/1111.</p>
-</div>
-</div>
-
-<div id="sidebare">
-<ul>
-
-<li> <a href="add.php">ADD</a></li>
-<li><a href="#delete.php">delete</a></li>
-<li> <a href="#modify.php">Modify</a></li>
-
-
-
-</ul>
-
-</div>
-
-<div id="data">
-
-<center> INFO</center>
-
+	</div>
 	
 
+	<?php  } ?>
 
-</div>
-
- </body>	
-</div>
-
-
+</body>
 
 
 </html>
---
+++ admin/php/login.php
@@ -0,0 +1,19 @@
+<style type="text/css">
+	fieldset{
+		width :300px;
+		margin:auto;
+	}
+	input{
+		width: 100%;
+	}
+</style>
+
+<fieldset>
+	<form method="POST">
+		<legend>login</legend>
+		<input placeholder="user id"  type="text" name="name" required="required"><br><br>
+		<input placeholder="passord"  type="password" name="pass" required="required"><br><br>
+		<input type="submit" name="">
+</form>
+	
+</fieldset>
